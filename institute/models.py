c7635456c357d30306766660a236cb3c8396c769
Robert Treharne <R.Treharne@liverpool.ac.uk>
2022-02-01 14:51:29 +0000

    update fnid as primary key

--- institute/models.py
+++ institute/models.py
@@ -3,7 +3,7 @@ from helpers.models import TrackingModel
 import uuid
 
 class Institute(TrackingModel):
-    fnid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    fnid = models.UUIDField(unique=True, primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
 
     def __str__(self):
