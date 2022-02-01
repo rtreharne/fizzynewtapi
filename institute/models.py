bbd4cf4f81680a5f8f45a13a3090d2257dca9ef4
Robert Treharne <R.Treharne@liverpool.ac.uk>
2022-02-01 14:00:24 +0000

    migrations update

--- institute/models.py
+++ institute/models.py
@@ -3,7 +3,7 @@ from helpers.models import TrackingModel
 import uuid
 
 class Institute(TrackingModel):
-    fnid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #fnid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
 
     def __str__(self):
