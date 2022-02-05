2c1eeb004bd022a4a417b796adb744fe0b9e3ef7
Robert Treharne <R.Treharne@liverpool.ac.uk>
2022-02-05 12:51:01 +0000

    institute domain create works

--- institute/serializers.py
+++ institute/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from institute.models import Institute, InstituteDomain
 from rest_framework import serializers
+from institute.models import Institute
 
 class InstituteSerializer(ModelSerializer):
+
+
     class Meta:
         model = Institute
         fields = ('fnid', 'name',)
@@ -10,6 +13,20 @@ class InstituteSerializer(ModelSerializer):
         read_only_fields = ['fnid']
 
 class InstituteDomainSerializer(ModelSerializer):
+
+
+    def validate(self, data):
+        print("validating data")
+        institute_fnid = data["institute_fnid"]
+        # Raise error if institute doesn't exist
+        try:
+            Institute.objects.get(fnid=institute_fnid)
+        except:
+            raise serializers.ValidationError("This institute does not exist")
+
+        return data
+
+
     class Meta:
         model = InstituteDomain
         fields = ('institute_fnid', 'domain', 'primary')
--- institute/views.py
+++ institute/views.py
@@ -23,30 +23,22 @@ class CreateInstituteDomainListAPIView(CreateAPIView):
     permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
-        print(serializer)
+        serializer.is_valid()
+        print(serializer.validated_data)
+        print("primary =", serializer.validated_data["primary"])
+
+        # Set primary field to False for all existing institute domains if new domain is primary
+        existing_domains = InstituteDomain.objects.filter(institute_fnid=serializer.validated_data["institute_fnid"])
+        if serializer.validated_data["primary"]:
+
+            if len(existing_domains) > 0:
+                existing_domains.update(primary=False)
+
+        elif len(existing_domains) == 0:
+            return serializer.save(primary=True)
+
         return serializer.save()
 
-    """
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        try:
-            institute_fnid = self.request.data["institute_fnid"]
-        except KeyError:
-            return Response({'error': 'institute_fnid required'}, status=status.HTTP_400_BAD_REQUEST)
-
-        try:
-            Institute.objects.get(fnid=institute_fnid)
-            if serializer["primary"]:
-                [x.update(primary=False) for x in InstituteDomain.objects.filter(institute_fnid=institute_fnid)]
-        except ObjectDoesNotExist:
-            return Response({'error': 'Institute does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        #self.perform_create(serializer)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    """
 
 class InstituteDomainListAPIView(ListAPIView):
     serializer_class=InstituteDomainSerializer
