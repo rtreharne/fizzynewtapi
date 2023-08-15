2be11c3c3895588a6b9018c68411afd7f914f445
Treharne <R.Treharne@liverpool.ac.uk>
2023-08-15 13:44:38 +0100

    update

--- fizzynewtapi/settings.py
+++ fizzynewtapi/settings.py
@@ -47,10 +47,10 @@ except:
         }
     }
 
-    import dj_database_url
+import dj_database_url
 
-    db_from_env = dj_database_url.config(conn_max_age=600)
-    DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
