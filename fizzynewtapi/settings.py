88bf557aa1317961b9c3290865dca1df52aaf1f7
Robert Treharne <R.Treharne@liverpool.ac.uk>
2023-08-15 16:36:10 +0100

    stop db_from_env

--- fizzynewtapi/settings.py
+++ fizzynewtapi/settings.py
@@ -50,10 +50,11 @@ try:
             }
         }
 
-        import dj_database_url
+        #import dj_database_url
 
-        db_from_env = dj_database_url.config(conn_max_age=600)
-        DATABASES['default'].update(db_from_env)
+        #db_from_env = dj_database_url.config(conn_max_age=600)
+        #DATABASES['default'].update(db_from_env)
+        print(DATABASES)
 except:
     DEBUG = True
     SECURE_SSL_REDIRECT = True
