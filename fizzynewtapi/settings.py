23892869c32c140898790f63b6dd4ec617256939
Robert Treharne <R.Treharne@liverpool.ac.uk>
2023-08-16 14:04:47 +0100

    Debug fix

--- fizzynewtapi/settings.py
+++ fizzynewtapi/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("FIZZYNEWTAPI_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 try:
     if sys.argv[1] == 'runserver':
@@ -154,13 +154,13 @@ WSGI_APPLICATION = 'fizzynewtapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
